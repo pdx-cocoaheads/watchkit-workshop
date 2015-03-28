76881cbe032d7322544fa2edc704d1065602785f
Ryan Arana <ryan.arana@gmail.com>
2015-03-28 08:25:25 -0700

    Rearrange results view a bit.

--- RoShamBo/RoShamBo/ViewController.swift
+++ RoShamBo/RoShamBo/ViewController.swift
@@ -22,16 +22,16 @@ class ViewController: UIViewController {
         }
     }
 
-    @IBOutlet weak var computerChoiceImage: UIImageView!
+    @IBOutlet weak var winnerImage: UIImageView!
+    @IBOutlet weak var loserImage: UIImageView!
     @IBOutlet weak var resultsLabel: UILabel!
-    @IBOutlet weak var computerChoiceLabel: UILabel!
+    @IBOutlet weak var verbLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        computerChoiceLabel.hidden = true
-
+        verbLabel.text = ""
         for c in Choice.all() {
             let btn: UIButton = getButtonForChoice(c)
             btn.setImage(UIImage(named: "\(c.name)-highlighted"), forState: UIControlState.Highlighted)
@@ -59,17 +59,30 @@ class ViewController: UIViewController {
 
     func playGame() {
         let compChoice = Choice.random()
-        computerChoiceImage.image = UIImage(named: "\(compChoice.name)-highlighted")
-        computerChoiceLabel.hidden = false
 
         var txt: String
         let r = Game(player1: playerChoice!, player2: compChoice).play()
 
         if r.draw {
             txt = "It's a draw. Try again!"
+
+            winnerImage.image = UIImage(named: "\(compChoice.name)-highlighted")
+            loserImage.image = UIImage(named: compChoice.name)
+            verbLabel.text = "=="
         } else {
-            let winStatement = r.winner == playerChoice ? "You win!" : "You lose!"
-            txt = "\(r.summary) \(winStatement)"
+            let playerWon = r.winner == playerChoice!
+            txt = playerWon ? "You win!" : "You lose!"
+
+            var winnerImageName = r.winner.name
+            var loserImageName = r.loser.name
+            if playerWon {
+                winnerImageName = "\(winnerImageName)-highlighted"
+            } else {
+                loserImageName = "\(loserImageName)-highlighted"
+            }
+            winnerImage.image = UIImage(named: winnerImageName)
+            loserImage.image = UIImage(named: loserImageName)
+            verbLabel.text = r.winner.verb(r.loser)
         }
 
         resultsLabel.text = txt
