6053739ef3ddb9ef0de8de8da8f265daf8dc48be
Ryan Arana <ryan.arana@gmail.com>
2015-03-29 15:34:18 -0700

    Add WatchKit App target

---
+++ RoShamBo/RoShamBo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  RoShamBo WatchKit Extension
+//
+//  Created by Ryan Arana on 3/29/15.
+//  Copyright (c) 2015 PDX-iOS. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
